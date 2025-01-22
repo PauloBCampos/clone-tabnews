69bf60fbb29d95519de8288c029659e33c13e687
PauloBCampos <paulocampos131@gmail.com>
2025-01-22 23:32:17 +0000

    Adicionando arquivos .js e arquivo .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
 import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("select 1+1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "são acima da média" });
 }
 
