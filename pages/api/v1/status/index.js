055a23afb4fbc2eba6ce4851254e98f88d2dd364
PauloBCampos <paulocampos131@gmail.com>
2025-02-08 00:13:15 +0000

    save code

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const maxConecPtg = await database.query("SHOW max_connections;");
+  const maxConecPtgValue = maxConecPtg.rows[0].max_connections;
+
+  const actConecPtg = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'salame_db';",
+  );
+  const actConecPtgValue = actConecPtg.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connection: parseInt(maxConecPtgValue),
+        curently_connection: actConecPtgValue,
+      },
+    },
+  });
 }
 
 export default status;
